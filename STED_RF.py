bc8caf0816f4036102a204b78a80bdd73c62e057
bsohplatform <bongseongoh@kier.re.kr>
2023-04-13 17:11:19 +0900

    commit. STED_RF
    - 기존 그리드서치 말고 베이지안 최적화로 하이퍼파라미터 튜닝

--- STED_RF.py
+++ STED_RF.py
@@ -2,29 +2,44 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split, GridSearchCV, cross_validate
 from sklearn.ensemble import RandomForestRegressor
+from bayes_opt import BayesianOptimization
 import joblib
 
+
+def rf_eval(max_depth, min_samples_split, min_samples_leaf):
+    params = {'n_estimators':100,'max_depth':int(round(max_depth)), 'min_samples_split':int(round(min_samples_split)), 'min_samples_leaf':int(round(min_samples_leaf))}
+    bas_csv = pd.read_csv('BAS_DB.csv')
+    input_list = ['fluid_h','Thi','Tho','Ph','mh','dTh','fluid_c','Tci','Tco','Pc','mc','dTc','DSH','DSC','comp_eff','Refrigerant',]
+    bas_data = bas_csv[input_list].to_numpy()
+    bas_target = bas_csv['COP'].to_numpy()
+    train_input, test_input, train_target, test_target = train_test_split(bas_data, bas_target, test_size = 0.05, random_state=42)
+    rf = RandomForestRegressor(**params, random_state=42,n_jobs=-1)
+    scores = cross_validate(rf, train_input, train_target, return_train_score=True, n_jobs=-1)
+    
+    return np.mean(scores['test_score'])
+
+pbounds = {'max_depth':(5, 20), 'min_samples_split':(2, 10), 'min_samples_leaf':(1, 10)}
+rf_bayesopt = BayesianOptimization(f=rf_eval, pbounds=pbounds, random_state=42)
+rf_bayesopt.maximize(init_point=5, n_iter=5)
+
+print(rf_bayesopt.max)
+
+max_params=rf_bayesopt.max['params']
+
+max_params['max_depth'] = int(round(max_params['max_depth']))
+max_params['min_samples_split'] = int(round(max_params['min_samples_split']))
+max_params['min_samples_leaf'] = int(round(max_params['min_samples_leaf']))
+
+print(max_params)
+
+
 bas_csv = pd.read_csv('BAS_DB.csv')
-
 input_list = ['fluid_h','Thi','Tho','Ph','mh','dTh','fluid_c','Tci','Tco','Pc','mc','dTc','DSH','DSC','comp_eff','Refrigerant',]
-
 bas_data = bas_csv[input_list].to_numpy()
 bas_target = bas_csv['COP'].to_numpy()
-
 train_input, test_input, train_target, test_target = train_test_split(bas_data, bas_target, test_size = 0.05, random_state=42)
+rf_max = RandomForestRegressor(**max_params, random_state=42, n_jobs=-1)
+rf_max.fit(train_input, train_target)
 
-rf = RandomForestRegressor(n_jobs=-1, n_estimators = 140, random_state=42)
-params = {'max_depth': [None, 15, 10, 7]}
-gs = GridSearchCV(rf, params, n_jobs=-1)
-gs.fit(train_input, train_target)
+joblib.dump(rf_max,'STED_RF.pkl')
 
-
-print(gs.best_params_)
-print(gs.cv_results_['mean_test_score'])
-
-
-#scores = cross_validate(rf, train_input, train_target, return_train_score=True, n_jobs=-1)
-#print(np.mean(scores['train_score']), np.mean(scores['test_score']))
-rf.fit(train_input, train_target)
-
-joblib.dump(rf,'STED_RF.pkl')
